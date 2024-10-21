b05da1a8b66516be32d50c70dcd23dea29a9c0a9
gblount00 <gabe.blount2999@gmail.com>
2024-10-21 08:11:24 -0400

    Add files via upload

--- HelpDesk_Multi-Tool.bat
+++ HelpDesk_Multi-Tool.bat
@@ -1,6 +1,6 @@
 @echo off
 title Help Desk Multi Tool
-Mode con lines=25 cols=90
+Mode con lines=20 cols=90
 chcp 65001 >nul
 color 0f
 
@@ -10,27 +10,24 @@ call :logo
 echo(
 echo(
 echo(
-echo 	1) Active Directory
-echo 	2) DHCP
-echo 	3) SCCM
-echo 	4) NinjaOne
-echo 	5) Avaya users
-echo 	6) Avaya Voicemail
-echo 	7) Adobe Admin Center
-echo 	8) Intune
-echo 	9) 365 admin center
+echo 		1) Active Directory                 a) Avaya Voicemail
+echo 		2) DHCP                             b) Adobe Admin Center
+echo 		3) SCCM                             c) Entra ID
+echo 		4) NinjaOne                         d) Intune 
+echo 		5) Avaya users                      e) 365 admin center
 echo(
-set /p program=Please select a program:_
+@echo off
+set /p program=.%program%               Please select a program:
 if %program%==1 goto 1
 if %program%==2 goto 2
 if %program%==3 goto 3
 if %program%==4 goto 4
 if %program%==5 goto 5
-if %program%==6 goto 6
-if %program%==7 goto 7
-if %program%==8 goto 8
-if %program%==9 goto 9
-if %program%==10 goto 10
+if /i %program%==a goto a
+if /i %program%==b goto b
+if /i %program%==d goto d
+if /i %program%==e goto e
+if /i %program%==c goto c
 goto :menu
 exit /b
 
@@ -55,19 +52,23 @@ goto :menu
 start "edge.exe" "https://10.17.11.40/network-login/"
 goto :menu 
 
-:6
+:a
 start "edge.exe" "https://10.17.11.18/#/splash?state=https:%2F%2F10.17.11.18%2F"
 goto :menu 
 
-:7
+:b
 start "edge.exe" "https://adminconsole.adobe.com/378DE09F56E821AA7F000101@AdobeOrg/overview"
 goto :menu 
 
-:8
+:c
+start "edge.exe" "https://entra.microsoft.com/?culture=en-us&country=us#home"
+goto :menu 
+
+:d
 start "edge.exe" "https://intune.microsoft.com/?feature.tokencaching=true&feature.internalgraphapiversion=true#home"
 goto :menu 
 
-:9
+:e
 start "edge.exe" "https://admin.microsoft.com/AdminPortal/home#/homepage"
 goto :menu 
 
